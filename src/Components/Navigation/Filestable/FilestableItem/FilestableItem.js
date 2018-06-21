95007fc33d6e391bfec4d4f59945abbdc76fbe6e
Corentin Ballot <corentin-ballot@users.noreply.github.com>
2018-06-21 21:06:22 +0200

    fix filestableitem buttons hundleclicks

--- src/Components/Navigation/Filestable/FilestableItem/FilestableItem.js
+++ src/Components/Navigation/Filestable/FilestableItem/FilestableItem.js
@@ -6,31 +6,37 @@ class FilestableItem extends Component {
     }
 
     handleEditNameClick = () => {
-        this.setState({ edit_name: true })
+        this.setState(prevState => ({
+            edit_name: true
+        }));
     }
 
     handleCancelEditNameClick = () => {
-        this.setState({ edit_name: false })
+        this.setState(prevState => ({
+            edit_name: false
+        }));
     }
 
     handleSubmitEditNameClick = () => {
-        this.setState({ edit_name: false })
+        this.setState(prevState => ({
+            edit_name: false
+        }));
     }
 
     render() {
         const { edit_name } = this.state
         return (
             <li className="filestable_content_item">
-                <div className="filestable_content_item_select"><i class="material-icons">{this.props.file.type === "dir"? "folder_open":"insert_drive_file"}</i></div>
+                <div className="filestable_content_item_select"><i className="material-icons">{this.props.file.type === "dir"? "folder_open":"insert_drive_file"}</i></div>
                 <div className="filestable_content_item_name">{this.props.file.name}</div>
                 <div className="filestable_content_item_icons">
-                    {this.props.file.type != "dir" && <a><i className="material-icons">file_download</i></a> }
-                    {edit_name === false && <button><i className="material-icons" >mode_edit</i></button>}
-                    {edit_name === true && <button><i className="material-icons" >done</i></button>}
-                    {edit_name === true && <button><i className="material-icons" >clear</i></button>}
+                    {this.props.file.type !== "dir" && <a><i className="material-icons">file_download</i></a> }
+                    {edit_name === false && <button onClick={this.handleEditNameClick}><i className="material-icons">mode_edit</i></button>}
+                    {edit_name === true  && <button onClick={this.handleSubmitEditNameClick}><i className="material-icons">done</i></button>}
+                    {edit_name === true  && <button onClick={this.handleCancelEditNameClick}><i className="material-icons">clear</i></button>}
                 </div>
                 <div className="filestable_content_item_lastupdate">{this.props.file.last_modif}</div>
-                <div className="filestable_content_item_size">{typeof this.props.file.size != undefined? this.props.file.size:"-"}</div>
+                <div className="filestable_content_item_size">{typeof this.props.file.size !== undefined? this.props.file.size:"-"}</div>
             </li>
         );
     }
