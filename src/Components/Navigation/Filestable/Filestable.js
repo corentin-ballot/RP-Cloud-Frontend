12478ef0628aaf78622c3fb5f8586524c0eb0843
Corentin Ballot <corentin.ballot@socgen.com>
2018-06-21 16:47:53 +0200

    add filestableitem component

--- src/Components/Navigation/Filestable/Filestable.js
+++ src/Components/Navigation/Filestable/Filestable.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './Filestable.css';
+import FilestableItem from './FilestableItem/FilestableItem.js';
 
 class Filestable extends Component {
     render() {
@@ -15,32 +16,8 @@ class Filestable extends Component {
                     <div className="filestable_header_size">Size</div>
                 </header>
                 <ol className="filestable_content">
-                    {this.props.filestable.dirs.map((item) => (
-                        <li className="filestable_content_item">
-                            <div className="filestable_content_item_select"><i class="material-icons">folder_open</i></div>
-                            <div className="filestable_content_item_name">{item.name}</div>
-                            <div className="filestable_content_item_icons">
-                                <button><i className="material-icons">mode_edit</i></button>
-                                <button><i className="material-icons">done</i></button>
-                                <button><i className="material-icons">clear</i></button>
-                            </div>
-                            <div className="filestable_content_item_lastupdate">{item.last_modif}</div>
-                            <div className="filestable_content_item_size">-</div>
-                        </li>
-                    ))}
-                    {this.props.filestable.files.map((item) => (
-                        <li class="filestable_content_item">
-                            <div className="filestable_content_item_select"><i class="material-icons">insert_drive_file</i></div>
-                            <div className="filestable_content_item_name">{item.name}</div>
-                            <div className="filestable_content_item_icons">
-                                <a><i className="material-icons">file_download</i></a>
-                                <button className="mdl-button"><i class="material-icons">mode_edit</i></button>
-                                <button className="mdl-button"><i class="material-icons">done</i></button>
-                                <button className="mdl-button"><i class="material-icons">clear</i></button>
-                            </div>
-                            <div className="filestable_content_item_lastupdate">{item.last_modif}</div>
-                            <div className="filestable_content_item_size">{item.size}</div>
-                        </li>
+                    {this.props.filestable.dirs.concat(this.props.filestable.files).map((item) => (
+                        <FilestableItem file={item} />
                     ))}
                 </ol>
             </div>
