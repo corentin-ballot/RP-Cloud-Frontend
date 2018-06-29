a0be4c63c1d97ae241d81bdf861d3da6200bfd1f
Corentin Ballot <corentin.ballot@gmail.com>
2018-06-29 21:06:36 +0200

    add baseroute to item url

--- src/Components/Navigation/Filestable/Filestable.js
+++ src/Components/Navigation/Filestable/Filestable.js
@@ -10,14 +10,18 @@ class Filestable extends Component {
     }
 
     getFilesinfos() {
-        return [
+        return ([
             {name:".","url":"/.",type:"dir",last_modif:"14/12/2017 20:15"},
             {name:"..","url":"/..",type:"dir",last_modif:"23/05/2018 19:08"},
             {name:"Documents","url":"/Documents",type:"dir",last_modif:"12/11/2017 15:33"},
             {name:"markdown_cheatsheet.md",type:"file",url:"/markdown_cheatsheet.md",size:"2,82 Ko",last_modif:"05/06/2018 19:02"},
             {name:"notes.md",type:"file",url:"/notes.md",size:"2,85 Ko",last_modif:"20/12/2017 8:53"},
             {name:"speedshare",type:"file",url:"/speedshare",size:"245 o",last_modif:"26/09/2017 8:51"}
-        ]
+        ].map((item, i, breadcrumb) => {
+            if(item.type === "dir")
+                item.url = this.props.baseroute + item.url;
+            return item;
+        }));
     }
 
     handleEditNameClick = (file) => {
