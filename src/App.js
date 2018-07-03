a45a03e859295e93678780191bb8cd2809eafd75
Corentin Ballot <corentin.ballot@gmail.com>
2018-07-03 13:31:05 +0200

    add route change listener

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 
 class App extends Component {
+  componentWillMount() {
+    this.unlisten = this.props.history.listen((location, action) => {
+      console.log("Route change (" + action + "). New location : " + location.pathname + location.search + location.hash);
+      switch (action) {
+        case 'PUSH' : // location pathname change
+        /* TODO : update file table and breadcrumb on route change using ajax request */
+        break;
+        case 'POP' : // location hash change
+        /* TODO : update file preview */
+        break;
+        default: break;
+      }
+    });
+  }
+  componentWillUnmount() {
+      this.unlisten();
+  }
+
   render() {
     return (
       <div className="App">
