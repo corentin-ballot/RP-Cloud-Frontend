4e7d850a921f7a187e26a7d69091ec77d573adf0
Corentin Ballot <corentin-ballot@users.noreply.github.com>
2018-07-03 20:57:10 +0200

    change html class to react className

--- src/Components/Navigation/Breadcrumb/Breadcrumb.js
+++ src/Components/Navigation/Breadcrumb/Breadcrumb.js
@@ -22,7 +22,7 @@ class Breadcrumb extends Component {
                         // last one
                         return (
                             <li className="cloud_navigation_breadcrumb_item" key={item.route}>
-                                <button className="cloud_navigation_breadcrumb_item_dropdown" onClick={this.handleDropdownclick}>{i === 0 ? <i class="material-icons">cloud</i>:item.folderName}<i className="material-icons">keyboard_arrow_down</i></button>
+                                <button className="cloud_navigation_breadcrumb_item_dropdown" onClick={this.handleDropdownclick}>{i === 0 ? <i className="material-icons">cloud</i>:item.folderName}<i className="material-icons">keyboard_arrow_down</i></button>
                                 <ul className="cloud_navigation_breadcrumb_item_dropdown_menu" aria-expanded={dropdown_open}>
                                     <li><button>Download files</button></li>
                                     <li><button>Delete files</button></li>
