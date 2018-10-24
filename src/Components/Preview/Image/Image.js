475139c29b766f0ff6c59ff765a273e41bca6b2c
Corentin Ballot <corentin.ballot@gmail.com>
2018-10-24 17:24:23 +0200

    add image preview style

--- src/Components/Preview/Image/Image.js
+++ src/Components/Preview/Image/Image.js
@@ -6,8 +6,8 @@ class Image extends Component {
 
     render() {
         return (
-            <div>
-                <img src={this.props.url} alt={this.props.alt} />
+            <div className="cloud_preview_panel_item_image">
+                <a href={this.props.url}><img src={this.props.url} alt={this.props.alt} /></a>
             </div>
         );
     }
