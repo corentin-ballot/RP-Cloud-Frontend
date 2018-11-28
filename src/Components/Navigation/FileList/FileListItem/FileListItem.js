783bbb299a0144c7b09abb4e4e265daa6a6f9911
Corentin Ballot <corentin.ballot-ext@socgen.com>
2018-11-28 12:06:48 +0100

    refactor: files mtime and size review using convertion components

--- src/Components/Navigation/FileList/FileListItem/FileListItem.js
+++ src/Components/Navigation/FileList/FileListItem/FileListItem.js
@@ -5,6 +5,9 @@ import { renameFile, toggleFileSelect, enableFileEditName, disableFileEditName }
 import { previewFile } from '../../../../Redux/actions/preview';
 import { connect } from 'react-redux'
 
+import ReadableOctets from '../../../Conversion/ReadableOctets/ReadableOctets';
+import ReadableDate from '../../../Conversion/ReadableDate/ReadableDate';
+
 import './FileListItem.css';
 
 class FileListItem extends Component {
@@ -31,20 +34,20 @@ class FileListItem extends Component {
     render() {
         return (
             <li className="filestable_content_item">
-                <div className="filestable_content_item_select" onClick={() => this.handleSelectClick()}><i className="material-icons">{this.props.file.selected? "check_box":(this.props.file.type === "dir"? "folder":"insert_drive_file")}</i></div>
-                {(this.props.file.editName !== true) && (this.props.file.type === "dir"?(<Link to={this.props.file.url + window.location.hash} className="filestable_content_item_name">{this.props.file.name}</Link>):(<div className="filestable_content_item_name" onClick={() => this.handleFileClick()}>{this.props.file.name}</div>))}
+                <div className="filestable_content_item_select" onClick={() => this.handleSelectClick()}><i className="material-icons">{this.props.file.selected ? "check_box" : (this.props.file.type === "dir" ? "folder" : "insert_drive_file")}</i></div>
+                {(this.props.file.editName !== true) && (this.props.file.type === "dir" ? (<Link to={this.props.file.url + window.location.hash} className="filestable_content_item_name">{this.props.file.name}</Link>) : (<div className="filestable_content_item_name" onClick={() => this.handleFileClick()}>{this.props.file.name}</div>))}
                 {(this.props.file.editName === true) && (<form className="filestable_content_item_name">
-                    <input name="newfile" className="filestable_content_item_name_rename" id={"rename-" + this.props.file.url} type="text" defaultValue={this.props.file.url} ref={el => this.newurl=el} />
+                    <input name="newfile" className="filestable_content_item_name_rename" id={"rename-" + this.props.file.url} type="text" defaultValue={this.props.file.url} ref={el => this.newurl = el} />
                 </form>)}
                 <div className="filestable_content_item_icons">
-                    {(this.props.file.editName !== true) && this.props.file.type !== "dir" && <a className="filestable_content_item_icons_item" href={"http://localhost/web/app.php/api/cloud/downloadfile?fileurl=" + encodeURI(this.props.file.url)}><i className="material-icons">file_download</i></a> }
+                    {(this.props.file.editName !== true) && this.props.file.type !== "dir" && <a className="filestable_content_item_icons_item" href={"http://localhost/web/app.php/api/cloud/downloadfile?fileurl=" + encodeURI(this.props.file.url)}><i className="material-icons">file_download</i></a>}
                     {(this.props.file.editName !== true) && <button className="filestable_content_item_icons_item" onClick={() => this.handleEditNameClick()}><i className="material-icons">mode_edit</i></button>}
-                    {(this.props.file.editName === true ) && <button className="filestable_content_item_icons_item" onClick={() => this.handleSubmitEditNameClick(this.newurl.value)}><i className="material-icons">done</i></button>}
-                    {(this.props.file.editName === true ) && <button className="filestable_content_item_icons_item" onClick={() => this.handleCancelEditNameClick()}><i className="material-icons">clear</i></button>}
+                    {(this.props.file.editName === true) && <button className="filestable_content_item_icons_item" onClick={() => this.handleSubmitEditNameClick(this.newurl.value)}><i className="material-icons">done</i></button>}
+                    {(this.props.file.editName === true) && <button className="filestable_content_item_icons_item" onClick={() => this.handleCancelEditNameClick()}><i className="material-icons">clear</i></button>}
                 </div>
                 <div className="filestable_content_item_info">
-                    <div className="filestable_content_item_info_size">{typeof this.props.file.size === undefined? "-":this.props.file.size}</div>
-                    <div className="filestable_content_item_info_lastupdate">{this.props.file.last_modif}</div>
+                    <ReadableOctets className="filestable_content_item_info_size">{this.props.file.size}</ReadableOctets>
+                    <ReadableDate className="filestable_content_item_info_lastupdate">{this.props.file.mtime}</ReadableDate>
                 </div>
             </li>
         );
@@ -53,6 +56,6 @@ class FileListItem extends Component {
 
 const mapStateToProps = (state) => {
     return {}
-  };
-  
-  export default connect(mapStateToProps)(FileListItem);
+};
+
+export default connect(mapStateToProps)(FileListItem);
