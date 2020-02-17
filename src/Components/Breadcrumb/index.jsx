00402e6f2f7ccdbc6f0ea71cfa2343f2eead73ec
Corentin BALLOT <corentin.ballot-ext@socgen.com>
2020-02-17 14:58:13 +0100

    fix breadcrumb

--- src/Components/Breadcrumb/index.jsx
+++ src/Components/Breadcrumb/index.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux'
 import { toggleNewDir, toggleNewFile } from '../../Redux/actions/navigation';
 
 import Dropdown from '../Dropdown';
 
 class Breadcrumb extends Component {
-
     render() {
+        const last_item=this.props.data[this.props.data.length-1];
         return (
             <>
                 <div className="d-inline-block overflow-auto" style={{scrollbarWidth: 'thin',}}>
                     <ol aria-label="breadcrumb" className="breadcrumb flex-nowrap overflow-auto mb-0 pl-0 pr-0 bg-transparent" style={{width:"max-content"}}>
+                        {/* ITEMS */}
                         {this.props.data.map((item, index) => {
                             return (index===this.props.data.length-1)?<li className="breadcrumb-item text-nowrap" key={item.route}></li>:<li className="breadcrumb-item text-nowrap" key={item.route}><Link to={`${item.route}${window.location.hash}`} className="breadcrumb-item text-nowrap">{!item.route?"Home":item.folderName}</Link></li>
                         })}
                     </ol>
                 </div>
-                {this.props.data.length && <Dropdown className="breadcrumb-item active text-nowrap mr-auto d-inline-block" aria-current="page" tag="div" btn={{text:this.props.data[this.props.data.length-1].folderName, tag: "button", attrs:{className:"btn btn-outline-primary"}}} direction="right">
+                {/* LAST ITEM */}
+                {this.props.data.length && <Dropdown className="breadcrumb-item active text-nowrap mr-auto d-inline-block" aria-current="page" tag="div" btn={{text:!last_item.route?"Home":last_item.folderName, tag: "button", attrs:{className:"btn btn-outline-primary"}}} direction={!last_item.route?"left":"right"}>
                     <button className="dropdown-item" onClick={() => this.handleNewFileClicked()}>New file</button>
                     <button className="dropdown-item" onClick={() => this.handleNewDirClicked()}>New folder</button>
                     <div className="dropdown-divider"></div>
@@ -40,4 +43,6 @@ class Breadcrumb extends Component {
     }
 }
 
-export default Breadcrumb;
+const mapStateToProps = (state) => {return {}};
+  
+export default connect(mapStateToProps)(Breadcrumb);
